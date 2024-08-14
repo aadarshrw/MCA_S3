f15ab115bd083c13d07ef3136402bcec0ca20d90
Aadarsh Warrier <126311721+aadarshrw@users.noreply.github.com>
2024-08-14 10:05:31 +0530

    Add files via upload

---
+++ MAD/LoginForm/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    Button login;
+    EditText username, password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.user);
+        password = (EditText) findViewById(R.id.pwd);
+        login = (Button)findViewById(R.id.LoginButton);
+        login.setOnClickListener(this);
+    }
+    public void onClick(View view)
+    {
+        if (view.getId()==R.id.LoginButton)
+        {
+           String a = username.getText().toString();
+           String b = password.getText().toString();
+           if(a.equals("Aadarsh") && b.equals("aadarsh123"))
+            {
+                Toast.makeText(this,"Login Successful",Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(this,"Login Failed",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
